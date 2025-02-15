1e07bc49f232ea670930d77e558a9dbf9b7bb6d2
HertonMabongue <hertoncabral04@gmail.com>
2025-02-15 23:24:49 +0200

    basic file layout/to be improved

---
+++ src/client/Client.java
@@ -0,0 +1,22 @@
+import java.io.IOExceptionimport java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Main Client class represents a chat client in the messaging application.
+ * This class handles communication with the server, sending and receiving messages,
+ * and managing the client’s connection.
+ * 
+ * @author idk
+ */
+public class Client {
+  
+  /**
+   * Initializes a new client instance.
+   * Handles connection setup and user authentication.
+   */
+  public Client() {
+      // Implementation goes here
+  }
+}
+
